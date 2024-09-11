ce5cf24131c62d57f624b47fc8e25ed31f8ed4ac
angelvec <angelmystic15@gmail.com>
2024-09-11 20:26:13 +1200

    update on cart

--- src/components/CartFoods.js
+++ src/components/CartFoods.js
@@ -12,8 +12,7 @@ function CartFoods(props)
 
     return (
         <>
-            <h3>{foodInfo.title}</h3>
-            <p>{quantity}</p>
+            <h3>{quantity}x {foodInfo.title}</h3>
             <p>${(quantity * foodInfo.price).toFixed(2)} </p>
             <Button size='sm' onClick={() => cart.removeAllFromCart(id)}>Remove</Button>
             <hr></hr>
